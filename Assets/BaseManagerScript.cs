ff007d3d02e1130053591936e1c79ab311768f12
Fabien <fabone248@gmail.com>
2014-05-22 09:55:22 +0200

    Movement

---
+++ Assets/BaseManagerScript.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BaseManagerScript : MonoBehaviour {
+
+	public List<GameObject> listOfSpawnPoints = new List<GameObject>();
+	public int nbUnitToSpawn;
+	private List<GameObject> listOfUnits = new List<GameObject>();
+	private int unitCounter;
+	private int currentSpawnPoint;
+	/*[HideInInspector]*/ public List<GameObject> listOfBases = new List<GameObject>();
+	
+	// Use this for initialization
+	void Start () {
+		
+		unitCounter = 0;
+		currentSpawnPoint = 0;
+		
+		GameObject[] bases = GameObject.FindGameObjectsWithTag("Base");
+		
+		foreach(GameObject baseInScene in bases)
+		{
+			if(baseInScene != this.gameObject)
+			{
+				listOfBases.Add(baseInScene);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(unitCounter < nbUnitToSpawn)
+		{
+			int i=0;
+			Debug.Log(this.name + " unitCounter:"+unitCounter+"  nbUnitToSpawn:"+nbUnitToSpawn);
+			for(i = unitCounter; i < nbUnitToSpawn; ++i)
+			{
+				GameObject unit = (GameObject)Instantiate(Resources.Load("Prefabs/Unit"));
+				unit.transform.position = listOfSpawnPoints[currentSpawnPoint++].transform.position;
+
+				if(currentSpawnPoint >= listOfSpawnPoints.Count)
+					currentSpawnPoint = 0;
+				
+				unit.GetComponent<MovementScript>().motherBase = this.gameObject;	
+				//unit.GetComponent<MovementScript>().enemy = GetClosestEnemy(unit.transform.position);
+			}
+			unitCounter = i;
+		}
+	}
+	
+	
+	public GameObject GetClosestEnemy(Vector3 position)
+	{
+		GameObject closest = null;
+		float distance = Mathf.Infinity;
+		float currentDistance = 0.0f;
+		
+		for(int i = 0; i < listOfBases.Count; ++i)
+		{
+		
+			for(int j = 0; j < listOfBases[i].GetComponent<BaseManagerScript>().getNumberOfUnit(); ++j)
+			{
+				currentDistance = Vector3.Distance(position, listOfBases[i].GetComponent<BaseManagerScript>().listOfUnits[j].transform.position);
+				
+				if(currentDistance < distance)
+				{
+					distance = currentDistance;
+					closest = listOfBases[i].GetComponent<BaseManagerScript>().listOfUnits[j];
+				}
+			}
+		}
+		
+		return closest;
+	}
+	
+	public int getNumberOfUnit()
+	{
+		//Debug.Log("fct:" + unitCounter);
+		return unitCounter;
+	}
+	
+	
+}
---
+++ Assets/MovementScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+
+	public GameObject enemy;
+	public float speedMovement;
+	public GameObject motherBase;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		this.rigidbody.velocity = new Vector3(0.0f, this.rigidbody.velocity.y, 0.0f);
+		
+		if(enemy == null)
+			enemy = motherBase.GetComponent<BaseManagerScript>().GetClosestEnemy(this.transform.position);
+		
+		if(enemy != null)
+		{
+			this.transform.LookAt(enemy.transform.position);
+			this.rigidbody.AddRelativeForce(0.0f,0.0f,1.0f * speedMovement * 10 * Time.deltaTime );
+		}
+	}
+	
+}
