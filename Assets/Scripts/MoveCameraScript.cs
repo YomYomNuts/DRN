28c86fec8f0f352fc513b773c6528b4491efc008
Noisette <noisette.guillaume@gmail.com>
2014-05-23 12:54:00 +0200

    Ajout du control de la camera

---
+++ Assets/Scripts/MoveCameraScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCameraScript : MonoBehaviour {
+
+    public float speed = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+	// Use this for initialization
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
